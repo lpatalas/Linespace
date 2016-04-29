8762d7bd9c8c11c4d92c0a26b0f444a9610637c9
Lukasz Patalas <lpatalas@gmail.com>
2016-04-29 23:46:48 +0200

    Implement scrolling with mouse

--- Linespace/main.ts
+++ Linespace/main.ts
@@ -7,8 +7,16 @@ module Linespace {
         y: number;
     }
 
+    const vec = function(x: number, y: number): Vec2D {
+        return { x, y };
+    }
+
     const vadd = function(a: Vec2D, b: Vec2D): Vec2D {
         return { x: a.x + b.x, y: a.y + b.y };
+    }
+
+    const vsub = function(a: Vec2D, b: Vec2D): Vec2D {
+        return vec(a.x - b.x, a.y - b.y);
     }
 
     const vmul = function(a: Vec2D, b: Vec2D): Vec2D {
@@ -17,6 +25,31 @@ module Linespace {
 
     const vsqrt = function(v: Vec2D): Vec2D {
         return { x: Math.sqrt(v.x), y: Math.sqrt(v.y) };
+    }
+
+    interface Transform2D {
+        m11: number;
+        m12: number;
+        m21: number;
+        m22: number;
+        dx: number;
+        dy: number;
+    }
+
+    const xform = function(m11: number, m12: number, m21: number, m22: number, dx: number, dy: number): Transform2D {
+        return { m11, m12, m21, m22, dx, dy };
+    }
+
+    const xidentity = function(): Transform2D {
+        return xform(1, 0, 0, 1, 0, 0);
+    }
+
+    const xtranslate = function(original: Transform2D, dx: number, dy: number): Transform2D {
+        return xform(original.m11, original.m12, original.m21, original.m22, original.dx + dx, original.dy + dy);
+    }
+
+    const xapply = function(t: Transform2D, context: CanvasRenderingContext2D) {
+        context.setTransform(t.m11, t.m12, t.m21, t.m22, t.dx, t.dy);
     }
 
     interface Orbit {
@@ -154,22 +187,61 @@ module Linespace {
             stars.forEach(star => drawPixel(vmul(star.position, getSize()), star.color));
         };
 
+        let currentTransform = xidentity();
+
         const drawObjects = function(time: number) {
+            xapply(currentTransform, context);
             drawStars();
             fillCircle(getCenter(), 30, '#ff8000');
             planets.forEach(planet => drawPlanet(time, planet));
+        };
+
+        const drawDebugOverlay = function() {
+            const transformJson = JSON.stringify(currentTransform);
+            context.fillStyle = 'red';
+            context.setTransform(2, 0, 0, 2, 0, 0);
+            context.fillText(transformJson, 10, 10);
         };
 
         const update = function(dt: number, time: number) {
             fitCanvasToWindow();
             clearCanvas();
             drawObjects(time);
+            drawDebugOverlay();
+        };
+
+        const hookMouseEvents = function() {
+            const body = document.getElementsByTagName('body')[0];
+            let initialMousePos: Vec2D = null;
+            let mousePressed = false;
+            let initialTransform = currentTransform;
+
+            body.addEventListener('mousedown', event => {
+                if (event.button == 0) {
+                    mousePressed = true;
+                    initialMousePos = vec(event.pageX, event.pageY);
+                    initialTransform = currentTransform;
+                }
+            });
+
+            body.addEventListener('mouseup', event => {
+                if (event.button == 0) {
+                    mousePressed = false;
+                }
+            });
+
+            body.addEventListener('mousemove', event => {
+                if (mousePressed) {
+                    const currentMousePos = vec(event.pageX, event.pageY);
+                    const offset = vsub(currentMousePos, initialMousePos);
+                    currentTransform = xtranslate(initialTransform, offset.x, offset.y);
+                    xapply(currentTransform, context);
+                }
+            });
         };
 
         const runMainLoop = function() {
-            
             const getCurrentTime = () => new Date().getTime() / 1000;
-
             let lastTime = getCurrentTime();
             
             setInterval(() => {
@@ -179,6 +251,7 @@ module Linespace {
             }, 33);
         };
 
+        hookMouseEvents();
         runMainLoop();
     }
 
