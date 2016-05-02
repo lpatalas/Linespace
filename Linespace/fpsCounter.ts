15fda89ff5f4883f3a74293cdc4a6b15ee763720
Lukasz Patalas <lpatalas@gmail.com>
2016-05-03 00:52:43 +0200

    Extract FPS counter to separate file

---
+++ Linespace/fpsCounter.ts
@@ -0,0 +1,25 @@
+﻿namespace Linespace {
+
+    export function createFpsCounter(debugDisplay: DebugDisplay) {
+        let frameCounter = 0;
+        let fpsMeasureStartTime: number;
+        const fpsMeasureInterval = 1;
+        let currentFps = 0;
+
+        return function(time: number) {
+            frameCounter++;
+
+            if (!fpsMeasureStartTime) {
+                fpsMeasureStartTime = time;
+            }
+            else if (time - fpsMeasureStartTime >= fpsMeasureInterval) {
+                currentFps = frameCounter / (time - fpsMeasureStartTime);
+                fpsMeasureStartTime = time;
+                frameCounter = 0;
+            }
+
+            debugDisplay.addText(`FPS: ${currentFps.toFixed(2)}`);
+        };
+    };
+
+}
--- Linespace/main.ts
+++ Linespace/main.ts
@@ -282,27 +282,7 @@
             planets.forEach(planet => drawPlanet(time, planet));
         };
 
-        const updateFpsCounter = (() => {
-            let frameCounter = 0;
-            let fpsMeasureStartTime: number;
-            const fpsMeasureInterval = 1;
-            let currentFps = 0;
-
-            return function(time: number) {
-                frameCounter++;
-
-                if (!fpsMeasureStartTime) {
-                    fpsMeasureStartTime = time;
-                }
-                else if (time - fpsMeasureStartTime >= fpsMeasureInterval) {
-                    currentFps = frameCounter / (time - fpsMeasureStartTime);
-                    fpsMeasureStartTime = time;
-                    frameCounter = 0;
-                }
-
-                debugDisplay.addText(`FPS: ${currentFps.toFixed(2)}`);
-            };
-        })();
+        const updateFpsCounter = createFpsCounter(debugDisplay);
 
         const update = function(dt: number, time: number) {
             debugDisplay.reset();
