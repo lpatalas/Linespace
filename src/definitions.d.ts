fe64e23d963c737f8a016b399e2c5078185a1f59
Lukasz Patalas <lpatalas@gmail.com>
2016-07-11 21:07:01 +0200

    Add tsconfig.json and fix duplicate identifier error

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -4,10 +4,6 @@ interface CanvasRenderingContext2D {
     ellipse(x: number, y: number, radiusX: number, radiusY: number, rotation: number, startAngle: number, endAngle: number, anticlockwise?: boolean): void;
 }
 
-interface Math {
-    seedrandom: prng;
-}
-
 interface WebGLDebugUtils {
     makeDebugContext(context: WebGLRenderingContext): WebGLRenderingContext;
 }
