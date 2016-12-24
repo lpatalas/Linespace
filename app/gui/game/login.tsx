4767395ab984fd0f253b4b9bfea4b06a9a0db9dc
Lukasz Patalas <lpatalas@gmail.com>
2016-12-24 13:08:38 +0100

    Improve event handling using arrow functions :)

--- app/gui/game/login.tsx
+++ app/gui/game/login.tsx
@@ -15,16 +15,16 @@ export class LoginComponent extends React.Component<LoginProps, LoginState> {
         this.state = { isPopupVisible: false };
     }
 
-    onLogin() {
+    onLogin = () => {
         // if we are logged in redirect to the game
         browserHistory.push('/game');
     }
 
-    onRegister() {
+    onRegister = () => {
         alert('contact us in case of new account!');
     }
 
-    onPopup() {
+    onPopup = () => {
         this.setState({ isPopupVisible: !this.state.isPopupVisible });
     }
 
@@ -35,13 +35,13 @@ export class LoginComponent extends React.Component<LoginProps, LoginState> {
                     <li><input type="text" /></li>
                     <li><input type="password" /></li>
                     <li>
-                        <button type="submit" onClick={() => this.onLogin()}>Login</button>
-                        or <a href="#" onClick={() => this.onRegister()}>Register for free!</a>
+                        <button type="submit" onClick={this.onLogin}>Login</button>
+                        or <a href="#" onClick={this.onRegister}>Register for free!</a>
                     </li>
-                    <li><a href="#" onClick={() => this.onPopup()}>show popup</a></li>
+                    <li><a href="#" onClick={this.onPopup}>show popup</a></li>
                 </ul>
                 {
-                    this.state.isPopupVisible ? <PopupComponent title="test" /> : null
+                    this.state.isPopupVisible && <PopupComponent title="test" />
                 }
 
             </div>
